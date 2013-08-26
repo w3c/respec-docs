4b6e0b36fc2c394ae2f9b90a0453f54cfcba3d4f
Robin Berjon <robin@berjon.com>
2013-08-26 18:57:22 +0200

    move output and use async

--- tools/build.js
+++ tools/build.js
@@ -3,36 +3,45 @@
 var r    = require("../node_modules/respec/tools/r")
 ,   fs   = require("fs")
 ,   pth  = require("path")
+,   async = require("async")
 ,   outPath = pth.join(__dirname, "..")
 ,   srcPath = pth.join(__dirname, "../src")
 ;
 
 
 // build custom Bootstrap
-// build Beryl ReSpec
 // build page with all configurations
 // copy pages in the right templates
 
-var config = {
-    baseUrl:    pth.join(__dirname, "../beryl")
-// ,   optimize:   "none"
-,   paths:  {
-        requireLib: "../node_modules/respec/js/require"
-    ,   simpleNode: "../node_modules/respec/js/simple-node"
-    ,   shortcut:   "../node_modules/respec/js/shortcut"
-    ,   domReady:   "../node_modules/respec/js/domReady"
-    ,   core:       "../node_modules/respec/js/core/"
-    ,   text:       "../node_modules/respec/js/text"
-    }
-,   name:       "beryl"
-,   include:    "requireLib simpleNode shortcut".split(" ")
-,   out:        pth.join(__dirname, "../beryl.js")
-,   inlineText: true
-,   preserveLicenseComments:    false
-};
-r.optimize(config, function () {
-    fs.writeFileSync(config.out,
-                    "/* Beryl - Robin Berjon, http://berjon.com/ (@robinberjon) */\n/* See original source for licenses: https://github.com/darobin/respec-docs. */\n" +
-                    fs.readFileSync(config.out));
+
+function buildBeryl (cb) {
+    var config = {
+        baseUrl:    pth.join(__dirname, "../beryl")
+    // ,   optimize:   "none"
+    ,   paths:  {
+            requireLib: "../node_modules/respec/js/require"
+        ,   simpleNode: "../node_modules/respec/js/simple-node"
+        ,   shortcut:   "../node_modules/respec/js/shortcut"
+        ,   domReady:   "../node_modules/respec/js/domReady"
+        ,   core:       "../node_modules/respec/js/core/"
+        ,   text:       "../node_modules/respec/js/text"
+        }
+    ,   name:       "beryl"
+    ,   include:    "requireLib simpleNode shortcut".split(" ")
+    ,   out:        pth.join(outPath, "js/beryl.min.js")
+    ,   inlineText: true
+    ,   preserveLicenseComments:    false
+    };
+    r.optimize(config, function () {
+        fs.writeFileSync(config.out,
+                        "/* Beryl - Robin Berjon, http://berjon.com/ (@robinberjon) */\n/* See original source for licenses: https://github.com/darobin/respec-docs. */\n" +
+                        fs.readFileSync(config.out, "utf8"), "utf8");
+        console.log("[OK] Beryl built");
+        cb();
+    });
+}
+
+async.series([buildBeryl], function (err) {
+    if (err) return console.log("ERROR:", err);
     console.log("OK!");
 });
