6996dcde1736a27b900cb0f3e743b2dfd0c4c097
Marcos Caceres <marcos@marcosc.com>
2016-05-20 16:11:44 +1000

    Fix(beryl-config): Remove silly subtitle

--- js/beryl-config.js
+++ js/beryl-config.js
@@ -1,21 +1,20 @@
 var respecConfig = {
-    subtitle:   "The stark raving awesome way to write specs."
-,   nav:  [
-        { url: "index.html", label: "Home" }
-    ,   { url: "guide.html", label: "User's Guide" }
-    ,   { url: "ref.html", label: "Reference" }
-    ,   { url: "dev.html", label: "Developers" }
-    ]
-,   afterEnd:   function () {
-        $.getJSON("js/refs.json", function (data) {
-            $("a:not([href])").each(function () {
-                var $a = $(this), opt = $a.text();
-                if (data[opt]) $a.attr("href", "ref.html#" + opt.toLowerCase());
-                else console.log("Reference to unknown configuration option: " + opt);
-            });
-        });
-    }
+  nav: [
+    { url: "index.html", label: "Home", },
+    { url: "guide.html", label: "User's Guide", },
+    { url: "dev.html", label: "Developers", },
+  ],
+  afterEnd: function() {
+    $.getJSON("js/refs.json", function(data) {
+      $("a:not([href])").each(function() {
+        var $a = $(this),
+          opt = $a.text();
+        if (data[opt]) $a.attr("href", "ref.html#" + opt.toLowerCase());
+        else console.log("Reference to unknown configuration option: " + opt);
+      });
+    });
+  }
 };
-window.mungeConf = function (utils, source) {
-    return source.replace("<section>", "<section class='well conf'>");
+window.mungeConf = function(utils, source) {
+  return source.replace("<section>", "<section class='well conf'>");
 };
